0cdebd075a63ec65465f4e8bd2a97a550d22fa1d
Fleet Release Manager <ay@mntw.re>
2023-03-13 12:11:53 +0100

    Output environment config during test runs

--- Makefile
+++ Makefile
@@ -1,5 +1,10 @@
 .EXPORT_ALL_VARIABLES:
 
+PWD = $(shell pwd)
+D8Y_AUTH_CONFIG_FILE ?= ${PWD}/tests/data/auth_config
+D8Y_AUTH_CACHE_FILE ?= ${PWD}/tests/data/auth
+D8Y_AUTH_CACHE_DATA ?= dummy
+
 install_pip_tools:
 	pip install pip-tools
 
@@ -14,4 +19,4 @@ install_develop: pip_compile pip_sync
 	python setup.py develop
 
 test:
-	pytest tests
+	pytest tests -s
--- tests/conftest.py
+++ tests/conftest.py
@@ -9,11 +9,15 @@ from . import config as test_config
 playlist_id: Optional[str] = None
 
 
+@pytest.fixture(autouse=True, scope="session")
+def env() -> None:
+    print()
+    print(f"D8Y_AUTH_CONFIG_FILE: {config.D8Y_AUTH_CONFIG_FILE}")
+    print(f"D8Y_AUTH_CACHE_FILE: {config.D8Y_AUTH_CACHE_FILE}")
+
+
 @pytest.fixture()
 def setup_auth() -> None:
-    if config.D8Y_AUTH_CACHE_FILE.exists() and config.D8Y_AUTH_CACHE_FILE.stat().st_size > 0:
-        return
-
     config.D8Y_AUTH_CACHE_FILE.write_text(test_config.D8Y_AUTH_CACHE_DATA)
 
 

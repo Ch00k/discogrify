7afdc59fdba590cd32ba75cef6fc894b88b008cc
Fleet Release Manager <ay@mntw.re>
2023-03-22 08:25:33 +0100

    Fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,25 @@
 import json
-from typing import Generator, Optional
+from typing import Callable, Generator, Optional
 
 import pytest
+from environs import Env
 
 from discogrify import cli, config
 
 playlist_id: Optional[str] = None
 
 
+@pytest.fixture(scope="session")
+def auth() -> None:
+    env = Env()
+
+    if auth_cache_data := env.str("D8Y_AUTH_CACHE_DATA", None):
+        print(f"Writing D8Y_AUTH_CACHE_DATA to {config.D8Y_AUTH_CACHE_FILE}")
+        config.D8Y_AUTH_CACHE_FILE.write_text(auth_cache_data)
+
+
 @pytest.fixture(autouse=True, scope="session")
-def env() -> None:
+def env(auth: Callable) -> None:
     print()
     print(f"D8Y_AUTH_CONFIG_FILE: {config.D8Y_AUTH_CONFIG_FILE}")
     print(f"D8Y_AUTH_CACHE_FILE: {config.D8Y_AUTH_CACHE_FILE}")
